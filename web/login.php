5982ba990d19923f7b941c6395400aa8865d9401
Jaime <jaime.escobar.mtz@gmail.com>
2019-12-10 00:40:06 -0700

    Logout added

--- web/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('controllers/usercontroller.php');
-    $controller = new usercontroller();
-
-    $result = $controller->login($_POST['email'], $_POST['password']);
-
-    echo $result;
